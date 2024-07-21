30e257d91063fd785c2d979fd078bf5f568d5436
alexpintodiaz <alexanderp143@hotmail.com>
2024-07-21 13:38:19 -0500

    add main modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AccountModule, AuthModule, TransactionModule],
 })
-export class AppModule {}
+export class AppModule { }
